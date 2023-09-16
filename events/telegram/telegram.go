e2d8f6adaf889d4586c13f8ad474273234d71567
Seva <lopachuk.seva@gmail.com>
2023-09-16 15:36:46 +0300

    event processor updates

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -2,13 +2,57 @@ package telegram
 
 import (
 	"link-adviser/clients/telegram"
+	"link-adviser/events"
+	"link-adviser/lib/e"
+	"link-adviser/storage"
 )
 
 type Processor struct {
-	tg     *telegram.Client
-	offset int
-	// storage
-
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
-// func New(client *telegram.Client, storage)
+func New(client *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      client,
+		storage: storage,
+	}
+}
+
+func (p *Processor) Fetch(limit int) ([]events.Event, error) {
+	update, err := p.tg.Updates(p.offset, limit)
+	if err != nil {
+		return nil, e.Wrap("can't get events", err)
+	}
+
+	res := make([]events.Event, 0, len(update))
+
+	for _, u := range update {
+		res = append(res, event(u))
+	}
+}
+
+func event(upd telegram.Update) events.Event {
+	updType := fetchType(upd)
+
+	res := events.Event{
+		Type: updType,
+		Text: fetchText(upd),
+	}
+}
+
+func fetchType(upd telegram.Update) events.Type {
+	if upd.Message == nil {
+		return events.Unknown
+	}
+
+	return events.Message
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+	return upd.Message.Text
+}
