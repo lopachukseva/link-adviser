25be478677df254df784c96e5314dacd69d23bd7
Seva <lopachuk.seva@gmail.com>
2023-09-24 16:33:04 +0300

    fetch update

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -13,6 +13,11 @@ type Processor struct {
 	storage storage.Storage
 }
 
+type Meta struct {
+	ChatID   int
+	Username string
+}
+
 func New(client *telegram.Client, storage storage.Storage) *Processor {
 	return &Processor{
 		tg:      client,
@@ -21,16 +26,24 @@ func New(client *telegram.Client, storage storage.Storage) *Processor {
 }
 
 func (p *Processor) Fetch(limit int) ([]events.Event, error) {
-	update, err := p.tg.Updates(p.offset, limit)
+	updates, err := p.tg.Updates(p.offset, limit)
 	if err != nil {
 		return nil, e.Wrap("can't get events", err)
 	}
 
-	res := make([]events.Event, 0, len(update))
+	if len(updates) == 0 {
+		return nil, nil
+	}
 
-	for _, u := range update {
+	res := make([]events.Event, 0, len(updates))
+
+	for _, u := range updates {
 		res = append(res, event(u))
 	}
+
+	p.offset = updates[len(updates)-1].ID + 1
+
+	return res, nil
 }
 
 func event(upd telegram.Update) events.Event {
@@ -40,6 +53,15 @@ func event(upd telegram.Update) events.Event {
 		Type: updType,
 		Text: fetchText(upd),
 	}
+
+	if updType == events.Message {
+		res.Meta = Meta{
+			ChatID:   upd.Message.Chat.ID,
+			Username: upd.Message.From.Username,
+		}
+	}
+
+	return res
 }
 
 func fetchType(upd telegram.Update) events.Type {
